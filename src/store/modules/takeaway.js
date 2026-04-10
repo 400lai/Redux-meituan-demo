5e3a18284b6aba5f979310a52087c2c471e37547
laiiiii <laiiiii@foxmail.com>
2026-04-10 14:45:07 +0800

    集成redux状态管理优化菜单组件：移除硬编码食物列表数据，在redux store中添加activeIndex状态字段，实现setActiveIndex reducer函数，导出setActiveIndex action创建器，使用useSelector和useDispatch钩子连接Redux store

--- src/store/modules/takeaway.js
+++ src/store/modules/takeaway.js
@@ -4,7 +4,10 @@ import axios from "axios";
 const foodStore = createSlice({
     name: 'foods',
     initialState: {
-        foodsList: []
+        // 商品列表数据
+        foodsList: [],
+        // 菜单激活下标值
+        activeIndex: 0
     },
     reducers: {
         // 定义一个名为 setFoodsList 的 reducer 函数
@@ -12,12 +15,21 @@ const foodStore = createSlice({
         setFoodsList(state, action) {
             // 将 action.payload（数据）赋给 state.foodsList
             state.foodsList = action.payload
+        },
+        // 菜单激活下标值 reducer
+        setActiveIndex(state, action) {
+            state.activeIndex = action.payload
         }
     }
 })
 
-// 从 foodStore 对象中解构出自动生成的 action 创建函数 setFoodsList
-const { setFoodsList } = foodStore.actions
+/**
+ * 获取状态更新方法
+ * 从 foodStore 的 actions 对象中解构出以下两个方法：
+ * - setFoodsList: 用于更新食品列表数据
+ * - setActiveIndex: 用于更新当前激活项的索引
+ */
+const { setFoodsList, setActiveIndex } = foodStore.actions
 // 定义一个异步 action 创建函数（Thunk），用于处理异步逻辑
 const fetchFoodsList = () => {
     // 返回一个异步函数，接收 dispatch 参数
@@ -29,7 +41,7 @@ const fetchFoodsList = () => {
 }
 
 // 导出异步获取食品列表的 thunk action creator
-export { fetchFoodsList }
+export { fetchFoodsList, setActiveIndex }
 // 导出 foodStore 的 reducer，用于配置 store
 const reducer = foodStore.reducer
 export default reducer
